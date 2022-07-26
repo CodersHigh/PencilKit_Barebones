18dabd202f20abefb411b5df552e13a7ecd7f1fd
이로운 <lru040409@gmail.com>
2022-07-26 14:54:49 +0900

    프로젝트 셋업

---
+++ PencilKit_Barebones/PencilKit_BarebonesApp.swift
@@ -0,0 +1,17 @@
+//
+//  PencilKitBarebonesApp.swift
+//  PencilKitBarebones
+//
+//  Created by 이로운 on 2022/07/26.
+//
+
+import SwiftUI
+
+@main
+struct PencilKitBarebonesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
